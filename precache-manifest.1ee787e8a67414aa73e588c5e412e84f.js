b84b2136396cec14b6bff347daf532ccd8b55372
ibulavsky <ibulavsky@gmail.com>
2020-01-28 14:50:24 +0300

    Updates

---
+++ precache-manifest.1ee787e8a67414aa73e588c5e412e84f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f61518a7c3597c96d177688dc5ca67",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "a2dcfe116eba8e34b0f7",
+    "url": "/social-network-react/static/css/main.560b7463.chunk.css"
+  },
+  {
+    "revision": "5285601619ee88cf6a4e",
+    "url": "/social-network-react/static/js/2.d7d50876.chunk.js"
+  },
+  {
+    "revision": "cb40438505bc3465f483",
+    "url": "/social-network-react/static/js/3.2bb9d5de.chunk.js"
+  },
+  {
+    "revision": "a2dcfe116eba8e34b0f7",
+    "url": "/social-network-react/static/js/main.89d4261d.chunk.js"
+  },
+  {
+    "revision": "143447381ea626c5ce9f",
+    "url": "/social-network-react/static/js/runtime-main.747fd44d.js"
+  },
+  {
+    "revision": "54d5c2637bbc304eba3c272a13dba805",
+    "url": "/social-network-react/static/media/false-tick.54d5c263.svg"
+  },
+  {
+    "revision": "dd257cf15a679c3330c7ea53a31eb300",
+    "url": "/social-network-react/static/media/preloader.dd257cf1.svg"
+  },
+  {
+    "revision": "9fcfd417fc66dd1ead3d6a87ebddc03b",
+    "url": "/social-network-react/static/media/right-tick.9fcfd417.svg"
+  },
+  {
+    "revision": "b6a2af8d60a07a3daac68139dbad98d4",
+    "url": "/social-network-react/static/media/user.b6a2af8d.png"
+  }
+]);
